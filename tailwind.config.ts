9523a62be8cfaa68b885948f219ca2f99c2d1267
Vlad <vvllaadd1233345@gmail.co>
2024-03-15 22:20:55 +0100

    project setup

---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			fontSize: {
+				xl: "24px",
+			},
+			screens: {
+				mobile: "360px",
+				desktop: "1024px",
+			},
+			colors: {
+				primary: "#FFF0F0",
+				secondary1: "#EAEEF7",
+			},
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
